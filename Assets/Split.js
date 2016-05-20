66cd42542450eb596e1b65ed193589f1c2de8679
NoCultist <a.matoszko94@gmail.com>
2016-05-21 00:13:28 +0200

    cokolwiek

--- Assets/Split.js
+++ Assets/Split.js
@@ -10,22 +10,30 @@ function Start () {
 }
 
 var rot = 0;
+var colorex = 0;
 function Update () {
+colorex +=1;
+if (colorex == 100){
+	 this.GetComponent.<ParticleSystem>().startColor = new Color(Random.Range(0.0,1.0),Random.Range(0.0,1.0),Random.Range(0.0,1.0));
+	 colorex = 0;
+}
+
+
 	if (Input.GetKey("space")){
 		if(side == 0){
-			if(rot <= 45){
-				transform.rotation.z -= 0.04*angleSpeed/Time.deltaTime;
-				rot ++;
-				if(rot == 45) rot = 0;
-			}
+			//if(rot <= 45){
+			//	transform.rotation.z -= 0.04*angleSpeed/Time.deltaTime;
+			//	rot ++;
+			//	if(rot == 45) rot = 0;
+			//}
 			transform.position.x += 1*Xspeed/Time.deltaTime;
 		}
 		if(side == 1){
-			if(rot <= 45){
-				transform.rotation.z += 0.04*angleSpeed/Time.deltaTime;
-				rot ++;
-				if(rot == 45) rot = 0;
-			}
+			//if(rot <= 45){
+			//	transform.rotation.z += 0.04*angleSpeed/Time.deltaTime;
+			//	rot ++;
+			//	if(rot == 45) rot = 0;
+			//}
 			transform.position.x -= 1*Xspeed/Time.deltaTime;
 		}
 	}
@@ -33,13 +41,13 @@ function Update () {
 	pos = transform.position.x;
 		if (pos > positor) {
 		if(side == 0){
-			transform.rotation.z += 0.08*angleSpeed/Time.deltaTime;
+			//transform.rotation.z += 0.08*angleSpeed/Time.deltaTime;
 			transform.position.x -= 1*Xspeed/Time.deltaTime;
 		}
 	}
 	else if (pos < positor) {
 		if(side == 1){
-			transform.rotation.z -= 0.08*angleSpeed/Time.deltaTime;
+		//	transform.rotation.z -= 0.08*angleSpeed/Time.deltaTime;
 			transform.position.x += 1*Xspeed/Time.deltaTime;
 		}
 	}
